0e96fdfc2568a1e02b836c39a2c423b65f501424
vishwjeet-ujgare <105901362+vishwjeet-ujgare@users.noreply.github.com>
2024-04-27 01:31:03 +0530

    Created and added firt code  in git repo
    First comment

---
+++ hello_cdac.java
@@ -0,0 +1,8 @@
+
+
+class hello_cdac {
+    public static void main(String[] args) {
+        System.out.println("Hello welcome to cadac");
+        System.out.println("Hello from  C-DAC ");	
+    }
+}
